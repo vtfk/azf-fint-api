b61395fdd9b83aefc5bc8862627b5cf3286bfcd7
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-06-22 21:39:01 +0200

    Added templates

---
+++ data/GraphQL/schema/person.js
@@ -0,0 +1,128 @@
+module.exports = {
+  query: `query fodselsnummer($fodselsnummer: String) {
+    person(fodselsnummer: $fodselsnummer) {
+      navn {
+        fornavn
+        mellomnavn
+        etternavn
+      }
+      kontaktinformasjon {
+        epostadresse
+        mobiltelefonnummer
+        telefonnummer
+      }
+      fodselsdato
+      fodselsnummer {
+        identifikatorverdi
+      }
+      kjonn {
+        kode
+      }
+      bostedsadresse {
+        adresselinje
+        postnummer
+        poststed
+      }
+      elev {
+        elevforhold {
+          skole {
+            navn
+          }
+          basisgruppe {
+            navn
+            trinn {
+              navn
+            }
+            termin {
+              navn
+            }
+            skolear {
+              navn
+            }
+            periode {
+              start
+              slutt
+            }
+          }
+          undervisningsgruppe {
+            navn
+            periode {
+              start
+              slutt
+            }
+            skolear {
+              navn
+            }
+            skole {
+              navn
+            }
+          }
+          kontaktlarergruppe {
+            navn
+            undervisningsforhold {
+              skoleressurs {
+                person {
+                  fodselsnummer {
+                    identifikatorverdi
+                  }
+                  navn {
+                    fornavn
+                    mellomnavn
+                    etternavn
+                  }
+                  kontaktinformasjon {
+                    epostadresse
+                    mobiltelefonnummer
+                    telefonnummer
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      personalressurs {
+        arbeidsforhold {
+          stillingstittel
+          ansettelsesprosent
+          arbeidsforholdsperiode {
+            start
+            slutt
+          }
+          hovedstilling
+          undervisningsforhold {
+            basisgruppe {
+              navn
+              trinn {
+                navn
+              }
+              termin {
+                navn
+              }
+              skolear {
+                navn
+              }
+              periode {
+                start
+                slutt
+              }
+            }
+            undervisningsgruppe {
+              navn
+              periode {
+                start
+                slutt
+              }
+              skolear {
+                navn
+              }
+              skole {
+                navn
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+}
